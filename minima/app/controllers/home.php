fdd8c61b2fc9f70806e24fd8856eee81082b24df
DipeshRamStha <visit.dipesh933@gmail.com>
2024-02-26 18:08:26 -0600

    defined view method in home php file

--- minima/app/controllers/home.php
+++ minima/app/controllers/home.php
@@ -4,6 +4,17 @@ Class Home
 {
   function index()
   {
-    echo "home page index method";
+    $this->view("home");
+  }
+
+  function view($view)
+  {
+    if(file_exists("../app/views/" . $view . ".php"))
+    {
+      include "../app/views/" . $view . ".php";
+    } else {
+      include "../app/views/404.php";
+    }
+
   }
 }
