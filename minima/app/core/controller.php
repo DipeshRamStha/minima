8c194af2990cc758a3d9f0b9e8349490df2f1a88
DipeshRamStha <visit.dipesh933@gmail.com>
2024-02-27 23:28:41 -0600

    Created the loalModel function and defined in controller php file

--- minima/app/core/controller.php
+++ minima/app/core/controller.php
@@ -10,6 +10,16 @@ Class Controller
     } else {
       include "../app/views/404.php";
     }
+  }
 
+  protected function loadModel($model)
+  {
+    if(file_exists("../app/models/" . $model . ".php"))
+    {
+      include "../app/models/" . $model . ".php";
+      return $model = new $model;
+    }
+
+    return false;
   }
 }
