ac1a5b740f875f05f9e5bbf2144fb5caabc232a0
DipeshRamStha <visit.dipesh933@gmail.com>
2024-02-28 11:12:50 -0600

    defined some important constants in config.php file

--- minima/app/core/config.php
+++ minima/app/core/config.php
@@ -1,3 +1,37 @@
 <?php
 
+// set website title
 define("WEBSITE_NAME", "My Website");
+
+// set database variables
+
+define('DB_TYPE', 'mysql');
+define('DB_NAME', 'mvc_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_HOST', 'localhost');
+
+/* protocol type http or https */
+define('PROTOCAL', 'http');
+
+/* root and asset paths*/
+
+$path = str_replace("\\", "/", PROTOCAL ."://" . $_SERVER['SERVER_NAME'] . __DIR__ . "/");
+$path = str_replace($_SERVER['DOCUMENT_ROOT'], "", $path);
+
+define('ROOT', str_replace("app/core", "public", $path));
+define('ASSETS', str_replace("app/core", "public/assets", $path));
+
+/*
+set to true to allow error reporting
+set to false when we upload online to stop error reporting
+*/
+
+define('DEBUG', true);
+
+if(DEBUG)
+{
+  ini_set("display_errors", 1);
+}else {
+  ini_set("display_errors", 0);
+}
