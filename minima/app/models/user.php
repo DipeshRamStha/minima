87b2b1f1bf3262e44cfbea68261b9ef5f3c1ab21
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-03 06:06:27 -0600

    Defined the login signup and check logged in function in the user file under models folder

--- minima/app/models/user.php
+++ minima/app/models/user.php
@@ -8,7 +8,7 @@ Class User
     $DB = new Database();
 
     $_SESSION['error'] = "";
-    if(isset($POST['username']) && isset($POST['username']))
+    if(isset($POST['username']) && isset($POST['password']))
     {
       $arr['username'] = $POST['username'];
       $arr['password'] = $POST['password'];
@@ -20,6 +20,7 @@ Class User
         // logged in
         $_SESSION['user_id'] = $data[0]->userid;
         $_SESSION['user_name'] = $data[0]->username;
+        $_SESSION['user_url'] = $data[0]->url_address;
       }else {
         $_SESSION['error'] = "wrong username or password";
       }
@@ -31,13 +32,52 @@ Class User
 
   }
 
-  function signup()
+  function signup($POST)
   {
+    $DB = new Database();
+
+    $_SESSION['error'] = "";
+    if(isset($POST['username']) && isset($POST['password']))
+    {
+      $arr['username'] = $POST['username'];
+      $arr['password'] = $POST['password'];
+      $arr['email'] = $POST['email'];
+
+      $query = "INSERT INTO users (username, password, email) VALUES (:username, :password, :email)";
+      $data = $DB->write($query, $arr);
+      if($data)
+      {
+        header("Location:" . ROOT . "login");
+        die;
+      }
+    }else{
+      $_SESSION['error'] = "pleases enter a valid username and password";
+    }
 
   }
 
-  function check_login()
+  function check_logged_in()
   {
+    $DB = new Database();
+
+    if(isset($_SESSION['user_url']))
+    {
+      $arr['user_url'] = $_SESSION['user_url'];
+
+      $query = "SELECT * FROM users WHERE url_address = :user_url LIMIT 1";
+      $data = $DB->read($query, $arr);
+      if(is_array($data))
+      {
+        // logged in
+        $_SESSION['user_id'] = $data[0]->userid;
+        $_SESSION['user_name'] = $data[0]->username;
+        $_SESSION['user_url'] = $data[0]->url_address;
+
+        return true;
+      }
+    }
+
+    return false;
 
   }
 }
