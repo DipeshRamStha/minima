3e87cb44b40decb4d17f68de34569a5a91bcf034
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-03 11:16:56 -0600

    Defined get random string max method inside the functions php file

--- minima/app/controllers/upload.php
+++ minima/app/controllers/upload.php
@@ -4,12 +4,19 @@ Class Upload extends Controller
 {
   function index()
   {
-    $data['page_title'] = "Upload";
-    $this->view("minima/upload", $data);
+    header("Location:" . ROOT . "upload/image");
+    die;
   }
 
   function image()
   {
+    $user = $this->loadModel("user");
+    if(!$result = $user->check_logged_in())
+    {
+      header("Location:". ROOT . "login");
+      die;
+
+    }
     $data['page_title'] = "Upload";
     $this->view("minima/upload", $data);
   }
