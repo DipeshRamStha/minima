acbce8905f44c238882731fd33848b766d036675
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-04 22:06:42 -0600

    Added the prev and next link in the index.php file and defined the generate_link and current_page_number functions

--- minima/app/models/posts.php
+++ minima/app/models/posts.php
@@ -3,7 +3,11 @@ Class Posts
 {
   function get_all()
   {
-    $query = "SELECT * FROM images ORDER BY id DESC LIMIT 12";
+    $page_number = isset($_GET['page']) ? $_GET['page'] : 1;
+
+    $limit = 2;
+    $offset = ($page_number - 1) * $limit;
+    $query = "SELECT * FROM images ORDER BY id DESC LIMIT $limit OFFSET $offset";
 
     $DB = new Database();
     $data = $DB->read($query);
@@ -11,6 +15,20 @@ Class Posts
     {
       return $data;
 
+    }
+    return false;
+  }
+
+  function get_one($link)
+  {
+    $query = "SELECT * FROM images WHERE url_address = :link LIMIT 1";
+    $arr['link'] = $link;
+
+    $DB = new Database();
+    $data = $DB->read($query, $arr);
+    if(is_array($data))
+    {
+      return $data[0];
     }
     return false;
   }
